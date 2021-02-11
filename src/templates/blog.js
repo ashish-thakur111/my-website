d153b02fd78b24ee0307d1271f63097933acff89
ashish-thakur111 <ashish.thakur11140@gmail.com>
2021-02-12 01:41:06 +0530

    Resolve the image not showing in blog issue

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,7 +1,11 @@
 import React from "react"
-import { graphql } from "gatsby"
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { graphql, Link } from "gatsby"
+import Img from "gatsby-image"
+import * as propTypes from "prop-types"
 
+// Import the new rendering and the render node definitions
+import { renderRichText } from "gatsby-source-contentful/rich-text"
+import { BLOCKS, INLINES } from "@contentful/rich-text-types"
 import Layout from "../components/hof/layout"
 
 // export const query = graphql`
@@ -16,42 +20,57 @@ import Layout from "../components/hof/layout"
 //   }
 // `
 
-export const query = graphql`
-  query($slug: String!) {
-    contentfulBlogPost(slug: { eq: $slug }) {
-      title
-      publishedDate(formatString: "MMMM Do, YYYY")
-      body {
-        raw
-      }
-    }
-  }
-`
-
+const options = {
+  renderNode: {
+    [INLINES.ENTRY_HYPERLINK]: ({
+      data: {
+        target: { slug, title },
+      },
+    }) => <Link to={slug}>{title}</Link>,
+    [BLOCKS.EMBEDDED_ASSET]: node => <Img {...node.data.target} />,
+  },
+}
 const Blog = props => {
-  const options = {
-    renderNode: {
-      "embedded-asset-block": (node) => {
-        const alt = node.data.target.fields.title['en-US']
-        const src = node.data.target.fields.title["en-US"].url
-        return <img alt={alt} src={src} />
-      }
-    }
-  }
-
+  const title = props.data.contentfulBlogPost.title
+  const body = props.data.contentfulBlogPost.body
   return (
     <Layout>
-      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <h1>{title}</h1>
       <p>{props.data.contentfulBlogPost.publishedDate}</p>
       {/* use in case of markdown */}
       {/* <div
         dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
       /> */}
-      {documentToReactComponents(
-        JSON.parse(props.data.contentfulBlogPost.body.raw), options
-      )}
+      {/* Render the Rich Text field data: */}
+      {body && renderRichText(body, options)}
     </Layout>
   )
 }
 
+Blog.propTypes = {
+  data: propTypes.object.isRequired,
+}
+
 export default Blog
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      slug
+      publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        raw
+        references {
+          contentful_id
+          __typename
+          ... on ContentfulAsset {
+            fluid(maxWidth: 1200) {
+              ...GatsbyContentfulFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`
